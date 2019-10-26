defef25bbf65ae438f8c54cda45410ddb6f2edb1
samlee <samlee2015jp@gmail.com>
2019-10-26 15:28:04 +0900

    add

---
+++ javaProjects/JavaBasic/src/com/sam/java/HelloWorld.java
@@ -0,0 +1,26 @@
+/**
+ * This is a Hello World java source. 
+ */
+package com.sam.java;
+
+/**
+ * @author Sam
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * 
+	 */
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, World.");
+	}
+
+}
